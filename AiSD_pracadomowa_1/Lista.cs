b5c6fb317a3a5300456257e24032653cab22f9e5
EAWitkiewicz <161207694+EAWitkiewicz@users.noreply.github.com>
2024-11-13 12:29:54 +0100

    klasa nodel i lista,add first i add last

---
+++ AiSD_pracadomowa_1/Lista.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AiSD_pracadomowa_1
+{
+    internal class Lista
+    {
+        //pierwszy element listy
+        public NodeL head;
+        //ostatni element listy
+        public NodeL tail;
+        //liczba elementow w liscie
+        public uint count;
+
+        //dodaje element na poczatku listy
+        public void AddFirst(int liczba)
+        {
+            //towrzenie nowego wezła z liczba w konstruktorze
+            var nowy = new NodeL(liczba);
+
+            //Ten krok przypisuje obecny pierwszy element listy (this.head) jako next dla nowego węzła.
+            nowy.next = this.head;
+            //Dzięki nowy.next = this.head mówimy, że nowy węzeł (nowy element) będzie wskazywał na obecny pierwszy element listy.
+            //Nowy węzeł wskazuje na dawną głowę listy. Takie przypisanie zapewnia, że dawny pierwszy element nie zostaje „porzucony” i jest dostępny jako następny element po nowym węźle.
+
+            //jesli w liscie sa juz jakies elementy(czy jest juz jakas glowa)
+            if (this.count > 0)
+            {
+                //this.head.prev = nowy; — dawny pierwszy element (wczesniejsza głowa) dostaje w polu prev referencję na nowy węzeł.(obecna glowe)
+                this.head.prev = nowy;
+            }
+            else
+            {//jesli na liscie nie ma elementów nasz nowy wezel ustawiamy jako ogon/ostani element listy
+                this.tail = nowy;
+            }
+            //i głową jest zarówno głową jak i ogonem
+            this.head = nowy;
+
+            //dodalismy element wiec inkrementujemy liste
+            count++;
+        }
+
+        public void AddLast(int liczba)
+        {
+            var nowy= new NodeL(liczba);
+
+            nowy.prev = this.tail;
+             if(this.count > 0)
+            {
+                this.tail.next = nowy;
+            }
+            else
+            {
+                this.head = nowy;
+            }
+            this.tail = nowy;
+            count++;
+        }
+    }
+}
---
+++ AiSD_pracadomowa_1/NodeL.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AiSD_pracadomowa_1
+{
+ //pojedynczy wezel listy dwukierunkowej
+    internal class NodeL
+    {
+        //referencja do nastepnego wezła na liście
+        public NodeL next;
+        //referancja do poprzedniego elementu na liscie
+        public NodeL prev;
+        //przechowuje dane (liczbe) przypisana do wezla
+        public int data;
+
+        //konstruktor przyjmuje liczbe
+        public NodeL(int liczba)
+        {
+            //przypisuje ją do do pola data
+            this.data = liczba;
+        }
+    }
+}
